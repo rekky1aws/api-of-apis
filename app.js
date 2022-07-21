4838334f2dddc3e9647f996eb382bc9661d808d2
rekkylaws <donatienhochart2@gmail.com>
2022-07-21 15:40:30 +0200

    Added SearchBar

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-const refreshElts = document.getElementsByClassName('refresh');
-const displayDiv = document.getElementsByTagName('main')[0];
-const pages = document.getElementById('pages-display');
+const refreshElts = document.querySelectorAll('.refresh');
+const displayDiv = document.querySelector('main');
+const pages = document.querySelector('#pages-display');
+const searchbar = document.querySelector("#searchbar");
 
 let page = 0;
 //console.log(displayDiv);
@@ -13,6 +14,10 @@ for (var i = 0; i < refreshElts.length; i++) {
 		displayData(data.entries);
 	});
 }
+
+searchbar.addEventListener("keyup", () => {
+	displayData(data.entries, page);
+});
 
 function displayData (dataArr, pageNum=0)
 {
@@ -28,10 +33,10 @@ function displayData (dataArr, pageNum=0)
 	
 	let maxPrint = Math.min((100*pageNum)+100, dataArr.length);
 	// console.log(maxPrint);
-	displayDiv.innerHTML="";
+	displayDiv.innerHTML = "";
 	for (var i = pageNum*100; i < maxPrint; i++) {
 		
-		let shouldDisplay = [false, false, false];
+		let shouldDisplay = [false, false, false, false];
 		
 		if (params.Cors === null) {
 			shouldDisplay[0] = true;
@@ -60,40 +65,140 @@ function displayData (dataArr, pageNum=0)
 			}
 		}
 
-		if (shouldDisplay[0] && shouldDisplay[1] && shouldDisplay[2])
+		if (searchbar.value.length >= 3) {
+			Object.keys(dataArr[i]).forEach (element => {
+				if (typeof dataArr[i][element] === "string")
+				{
+					if (dataArr[i][element].toLowerCase().indexOf(searchbar.value.toLowerCase()) >= 0) {
+						// console.log(element);
+						console.log(dataArr[i][element])
+						shouldDisplay[3] = true;
+					}
+				}
+			});
+		} else {
+			shouldDisplay[3] = true;
+		}
+
+		if (shouldDisplay[0] && shouldDisplay[1] && shouldDisplay[2] && shouldDisplay[3])
 		{
-			displayDiv.innerHTML += `
-			<div class="api-card">
-				<div class="api-name">
-					<span class="api-name-title underline">Nom : </span>
-					<span class="api-name-value">${dataArr[i].API}</span>
-				</div>
-				<div class="api-description">
-					<span class="api-description-title underline">Description : </span>
-					<span class="api-description-value">${dataArr[i].Description}</span>
-				</div>
-				<div class="api-category">
-					<span class="api-category-title underline">Categorie : </span>
-					<span class="api-category-value">${dataArr[i].Category}</span>
-				</div>
-				<div class="api-cors">
-					<span class="api-cors-title underline">Cors : </span>
-					<span class="api-cors-value">${dataArr[i].Cors}</span>
-				</div>
-				<div class="api-auth">
-					<span class="api-auth-title underline">Auth : </span>
-					<span class="api-auth-value">${dataArr[i].Auth}</span>
-				</div>
-				<div class="api-https">
-					<span class="api-https-title underline">HTTPS : </span>
-					<span class="api-https-value">${dataArr[i].HTTPS}</span>
-				</div>
-				<div class="api-https">
-					<span class="api-https-title underline">Lien : </span>
-					<a href="${dataArr[i].Link}" class="api-https-value">${dataArr[i].Link}</a>
-				</div>
-			</div>
-			`;
+			let apiCard = document.createElement('div');
+			apiCard.className = "api-card";
+
+			// Name
+			 let apiName = document.createElement('div');
+			 apiName.className = "api-name";
+
+			  let apiNameTitle = document.createElement('span');
+			  apiNameTitle.className = "api-name-title underline";
+			  apiNameTitle.innerHTML = "Nom : ";
+			  apiName.appendChild(apiNameTitle);
+
+			  let apiNameValue = document.createElement('span');
+			  apiNameValue.className = "api-name-value";
+			  apiNameValue.innerHTML = dataArr[i].API;
+			  apiName.appendChild(apiNameValue);
+
+		  	// Description
+			let apiDescription = document.createElement('div');
+			 apiDescription.className = "api-description";
+
+			  let apiDescriptionTitle = document.createElement('span');
+			  apiDescriptionTitle.className = "api-description-title underline";
+			  apiDescriptionTitle.innerHTML = "Descripton : ";
+			  apiDescription.appendChild(apiDescriptionTitle);
+
+			  let apiDescriptionValue = document.createElement('span');
+			  apiDescriptionValue.className = "api-description-value";
+			  apiDescriptionValue.innerHTML = dataArr[i].Description;
+			  apiDescription.appendChild(apiDescriptionValue);
+
+			// Category
+			 let apiCategory = document.createElement('div');
+			 apiCategory.className = "api-category";
+
+			  let apiCategoryTitle = document.createElement('span');
+			  apiCategoryTitle.className = "api-category-title underline";
+			  apiCategoryTitle.innerHTML = "Catégorie : ";
+			  apiCategory.appendChild(apiCategoryTitle);
+
+			  let apiCategoryValue = document.createElement('span');
+			  apiCategoryValue.className = "api-category-value";
+			  apiCategoryValue.innerHTML = dataArr[i].Category;
+			  apiCategory.appendChild(apiCategoryValue);
+
+			// CORS
+		  	 let apiCors = document.createElement('div');
+			 apiCors.className = "api-cors";
+
+			  let apiCorsTitle = document.createElement('span');
+			  apiCorsTitle.className = "api-cors-title underline";
+			  apiCorsTitle.innerHTML = "CORS : ";
+			  apiCors.appendChild(apiCorsTitle);
+
+			  let apiCorsValue = document.createElement('span');
+			  apiCorsValue.className = "api-cors-value";
+			  apiCorsValue.innerHTML = dataArr[i].Cors;
+			  apiCors.appendChild(apiCorsValue);
+
+			// Auth
+			 let apiAuth = document.createElement('div');
+			 apiAuth.className = "api-auth";
+
+			  let apiAuthTitle = document.createElement('span');
+			  apiAuthTitle.className = "api-auth-title underline";
+			  apiAuthTitle.innerHTML = "Auth : ";
+			  apiAuth.appendChild(apiAuthTitle);
+
+			  let apiAuthValue = document.createElement('span');
+			  apiAuthValue.className = "api-auth-value";
+			  if (dataArr[i].Auth) {
+				apiAuthValue.innerHTML = dataArr[i].Auth;
+			  } else {
+			  	apiAuthValue.innerHTML = "None"
+			  }
+			  apiAuth.appendChild(apiAuthValue);
+
+			// HTTPS
+			 let apiHTTPS = document.createElement('div');
+			 apiHTTPS.className = "api-https";
+
+			  let apiHTTPSTitle = document.createElement('span');
+			  apiHTTPSTitle.className = "api-https-title underline";
+			  apiHTTPSTitle.innerHTML = "HTTPS : ";
+			  apiHTTPS.appendChild(apiHTTPSTitle);
+
+			  let apiHTTPSValue = document.createElement('span');
+			  apiHTTPSValue.className = "api-https-value";
+			  apiHTTPSValue.innerHTML = dataArr[i].HTTPS;
+			  apiHTTPS.appendChild(apiHTTPSValue);
+
+			// Link
+			 let apiLink = document.createElement('div');
+			 apiLink.className = "api-link";
+
+			  let apiLinkTitle = document.createElement('span');
+			  apiLinkTitle.className = "api-link-title underline";
+			  apiLinkTitle.innerHTML = "Link : ";
+			  apiLink.appendChild(apiLinkTitle);
+
+			  let apiLinkValue = document.createElement('a');
+			  apiLinkValue.className = "api-link-value";
+			  apiLinkValue.innerHTML = dataArr[i].Link;
+			  apiLinkValue.href = dataArr[i].Link;
+			  apiLink.appendChild(apiLinkValue);
+
+			// Adding all infos to apiCard
+			 apiCard.appendChild(apiName);
+			 apiCard.appendChild(apiDescription);
+			 apiCard.appendChild(apiCategory);
+			 apiCard.appendChild(apiCors);
+			 apiCard.appendChild(apiAuth);
+			 apiCard.appendChild(apiHTTPS);
+			 apiCard.appendChild(apiLink);
+
+			// Adding apiCard to displayDiv.
+			displayDiv.appendChild(apiCard);
 		}
 	}
 }
@@ -103,7 +208,7 @@ async function loadAPI ()
 	try {
 		const response = await fetch("https://api.publicapis.org/entries");
 		data = await response.json();
-		// console.log(data);
+		console.log(data);
 
 		for (var i = 0; i < Math.floor(data.count/100) + 1; i++) {
 			pages.innerHTML += `<button onclick="goToPage(${i});" class="round-button page-button">${i+1}</button>`;
